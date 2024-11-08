57b2a3b326749d86121bf272b46c33ef6c812797
Lenard H. De Luna <lenarddeluna@gmail.com>
2024-11-08 11:52:30 +0800

    Peys App code done

--- Peys App/index.php
+++ Peys App/index.php
@@ -1,11 +1,46 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Peys App</title>
 </head>
+
 <body>
-    
+
+    <h1>Peys App</h1>
+
+    <?php
+    // Capture form input with default values for size and border color
+    $size = !empty($_POST['size']) ? intval($_POST['size']) : 60;
+    $borderColor = !empty($_POST['borderColor']) ? htmlspecialchars($_POST['borderColor']) : '#000000';
+
+    // Define canvas dimensions based on size input
+    $canvasWidth = $canvasHeight = $size;
+    ?>
+
+    <form method="post">
+        <label for="size">Select Photo Size:</label>
+        <input type="range" id="size" name="size" min="10" max="100" value="<?php echo $size; ?>" step="10"
+            oninput="document.getElementById('sizeOutput').textContent = this.value">
+        <output id="sizeOutput"><?php echo $size; ?></output>
+
+        <br><br>
+
+        <label for="borderColor">Select Border Color:</label>
+        <input type="color" id="borderColor" name="borderColor" value="<?php echo $borderColor; ?>">
+
+        <br><br>
+
+        <button type="submit">Process</button>
+
+        <br><br>
+
+        <div class="image-container"
+            style="border: 3px solid <?php echo $borderColor; ?>; width: <?php echo $canvasWidth; ?>px; height: <?php echo $canvasHeight; ?>px; overflow: hidden;">
+            <img src="img/ghost.jpg" alt="Image for Canvas" width="<?php echo $canvasWidth; ?>" height="<?php echo $canvasHeight; ?>">
+        </div>
+    </form>
 </body>
 </html>
